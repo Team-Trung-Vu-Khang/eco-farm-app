d00a61192a93c2d5a22182133a30b75ae9eb8488
tranvux <tranvux.dev@gmail.com>
2026-01-22 11:18:00 +0700

    feat: add pages :hammer:

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
@@ -19,6 +20,11 @@ export default defineConfig({
       shared: [],
     }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   // base: "/farm",
   // build: { target: "esnext" },
   server: {
