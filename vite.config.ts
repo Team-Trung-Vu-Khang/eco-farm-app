50407a6804b662d8e6b390cbe1a2ec98ca7db7c5
Ethan <doank3442@gmail.com>
2026-01-23 23:22:04 +0700

    fix/deploy

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,6 @@ export default defineConfig({
       filename: "remoteEntry.js",
       exposes: {
         "./App": "./src/App.tsx",
-        "./FarmerFarmingWorkPage": "./src/pages/farmer/farming-work/index.tsx",
       },
       shared: [],
     }),
