a8c41307bcf04964cee84164316244491c993d9f
tankhang1 <doank3442@gmail.com>
2026-09-20 20:49:57 +0700

    fix: error reload

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { QueryClientProvider } from "@tanstack/react-query";
-import "./index.css";
 import App from "./App.tsx";
+import "./index.css";
 import { queryClient } from "./shared/config/query-client";
+
+window.addEventListener("vite:preloadError", (event) => {
+  event.preventDefault();
+  window.location.reload();
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
